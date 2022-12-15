1d671b7a3410a5b4fe6b84714f8020b3ba2c513e
孙鹏 <peng.sun@ximalaya.com>
2022-12-15 20:14:05 +0800

    new

--- main.py
+++ main.py
@@ -5,20 +5,9 @@
 
 import turtle,math
 
-x1,y1 = 100, 100
-x2,y2 = 100,-100
-x3,y3 = -100,-100
-x4,y4 = -100,100
+a = ""
 
-turtle.penup()
-turtle.goto(x1,y1)
-turtle.pendown()
-turtle.goto(x2,y2)
-turtle.goto(x3,y3)
-turtle.goto(x4,y4)
-
-distance1 = math.sqrt((x1-x4)**2+(y1-y4)**2)
-turtle.write(distance1)
+for i in range(10000000):
 
 
 
