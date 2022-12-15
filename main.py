e4db8147a46a16171e82458da3658e8a9634c0b9
孙鹏 <peng.sun@ximalaya.com>
2022-12-15 20:23:14 +0800

    字符串对比

--- main.py
+++ main.py
@@ -3,12 +3,29 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-import turtle,math
+import time
+
+time01 = time.time()
 
 a = ""
 
-for i in range(10000000):
+for i in range(100000):
+    a += "sxt"
 
+time02 = time.time()
 
+print ("运算时间：" + str(time02-time01))
+
+time03 = time.time()
+
+li = []
+for i in range(100000):
+    li.append("sxt")
+
+a = "".join(li)
+
+time04 = time.time()
+
+print ("运算时间：" + str(time04-time03))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
